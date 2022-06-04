641d70d55516591488f6cfdad84ad1ac15e4d5fe
Kamran Davar <kamran.davar.KD@GMAIL.COM>
2022-06-04 12:04:42 +0430

    more prrety colors

--- logic/hooks/themeCreator.ts
+++ logic/hooks/themeCreator.ts
@@ -3,32 +3,43 @@ import { createTheme } from "@mui/material/styles";
 import { yellow, grey } from "@mui/material/colors";
 import { Direction } from "@mui/material";
 
+export default function useTheme(
+  mode: "light" | "dark",
+  dir: Direction,
+  font: string
+) {
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: mode,
+          ...(mode === "light"
+            ? {
+               background:{
+                   paper: "#f5f5f7",
+               }
+            }
+            : {
+                // palette values for dark mode
+                background:{
+                    paper: "#18192e",
+                }
+              }),
 
-export default function useTheme(mode: "light" | "dark", dir: Direction, font: string) {
+          primary: {
+            main: yellow[800],
+          },
+          secondary: {
+            main: grey[50],
+          },
+        },
+        typography: {
+          fontFamily: font,
+        },
+        direction: dir,
+      }),
+    [mode, font, dir]
+  );
 
-    const theme = React.useMemo(
-        () =>
-            createTheme({
-                palette: {
-                    mode: mode,
-                    background: {
-                        default: "red",
-                    },
-                    primary: {
-                        main: yellow[800],
-                    },
-                    secondary: {
-                        main: grey[50]
-                    },
-                },
-                typography: {
-                    fontFamily: font,
-                },
-                direction: dir,
-            }),
-        [mode, font, dir]
-    );
-
-
-    return  theme ;
+  return theme;
 }
